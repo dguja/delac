3c6b1b8763b79af1e6062f2af296000a91449e7a
Antun Razum <antun.razum@fer.hr>
2014-12-22 13:23:13 +0100

    Projekt pretvoren u Maven projekt. Maknut kod za distribucije. Dodan Apache commons math3 library (za distribucije).

--- src/main/java/hr/fer/zemris/composite/generator/distributions/IDiscreteDistribution.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.zemris.composite.generator.distributions;
-
-public interface IDiscreteDistribution {
-
-  int nextNumber();
-
-}
---
+++ src/test/java/hr/fer/zemris/composite/generator/EmptyTest.java
@@ -0,0 +1,10 @@
+package hr.fer.zemris.composite.generator;
+
+import org.junit.Test;
+
+public class EmptyTest {
+
+  @Test
+  public void testTrue() {}
+
+}
