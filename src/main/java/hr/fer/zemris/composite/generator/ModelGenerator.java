cc3b75c9dd003acea4bb7302f56cba4e6e496a9b
Daniel <daniel.guja@gmail.com>
2015-02-08 22:36:31 +0100

    Pokriveni jos neki slucajevi, dovrsiti 7. i 8. korak

--- src/main/java/hr/fer/zemris/composite/generator/ModelGenerator.java
+++ src/main/java/hr/fer/zemris/composite/generator/ModelGenerator.java
@@ -12,9 +12,11 @@ import hr.fer.zemris.composite.generator.random.RandomUtilities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ModelGenerator {
 
@@ -69,7 +71,7 @@ public class ModelGenerator {
     for (int i = 0; i < originals.size(); i++) {
       inputs.add(copyInputs ? originals.get(i).clone() : originals.get(i));
     }
-    
+
     // 3
     final int k = discreteDistributions.get("d3").sample();
 
@@ -79,14 +81,15 @@ public class ModelGenerator {
     }
 
     for (int i = 0; i < k; i++) {
-      final List<AbstractNode> nodes;
+      List<AbstractNode> nodes = null;
       if (i == 0) {
         nodes = inputs;
       } else {
         // 6
+        List<AbstractNode> previousLevelNodes = nodes;
         nodes = createNodes(discreteDistributions.get("d11").sample());
         // 7
-        connectNodes(nodes, i, k, levelEdges);
+        nodes = connectNodes(nodes, i, k, levelEdges, previousLevelNodes);
       }
       // 4, 5
       createEdges(nodes, i, k, levelEdges);
@@ -95,45 +98,99 @@ public class ModelGenerator {
 
   }
 
-  private void connectNodes(List<AbstractNode> nodes, int currentLevel, int depthK,
-      List<Map<AbstractNode, Integer>> levelEdges) {
+  private List<AbstractNode> connectNodes(List<AbstractNode> nodes, int currentLevel, int depthK,
+      List<Map<AbstractNode, Integer>> levelEdges, List<AbstractNode> previousLevelNodes) {
 
     int l = nodes.size();
     int p = calculateNumberOfEdges(levelEdges.get(currentLevel));
 
     if (l == p) {
+      
       lEqualP(nodes, levelEdges, currentLevel, l);
     } else if (l < p) {
-      IDiscreteDistribution behaviorOfGenerator = new DiscreteDistributionLimiter(discreteDistributions.get("d12"), 0, 3);
+      
+      IDiscreteDistribution behaviorOfGenerator =
+          new DiscreteDistributionLimiter(discreteDistributions.get("d12"), 0, 3);
       switch (behaviorOfGenerator.sample()) {
         case 1:
-          nodes.addAll(createNodes(p-l));
+          nodes.addAll(createNodes(p - l));
           lEqualP(nodes, levelEdges, currentLevel, p);
           break;
         case 2:
-          
+          Map<AbstractNode, Integer> parents = levelEdges.get(currentLevel);
+          for (AbstractNode parent : parents.keySet()) {
+            int nodesToChoose = l;
+            List<AbstractNode> children = new LinkedList<>(nodes);
+            for (int i = parents.get(parent); i >= 0; i--) {
+
+              // ako roditelj ima vise veza nego djece na koje se moze povezati, odbaci visak veza
+              if (i > l) {
+                i = l;
+              }
+
+              AbstractNode child = children.remove(RandomProvider.getRandom().nextInt(nodesToChoose--));
+              child.getParents().add(parent);
+              parent.getChildren().add(child);
+            }
+          }
+          break;
         case 3:
+
       }
     } else {
-
+      
+      IDiscreteDistribution behaviorOfGenerator =
+          new DiscreteDistributionLimiter(discreteDistributions.get("d13"), 0, 2);
+      switch (behaviorOfGenerator.sample()) {
+        case 1:
+          
+          int sizeOfPreviousNodes = previousLevelNodes.size();
+          // stvori novih l-p veza
+          for (int i = l - p; i >= 0; i--) {
+            putEdgeInLevelEdges(levelEdges, currentLevel,
+                previousLevelNodes.get(RandomProvider.getRandom().nextInt(sizeOfPreviousNodes)));
+          }
+          lEqualP(nodes, levelEdges, currentLevel, l);
+          break;
+        case 2:
+          
+          Set<AbstractNode> newNodes = new HashSet<>();
+          Map<AbstractNode, Integer> parents = levelEdges.get(currentLevel);
+          for(AbstractNode parent : parents.keySet()) {
+            List<AbstractNode> children = new LinkedList<>(nodes);
+            int numberOfNodes = l;
+            for(int i = 0; i < parents.get(parent); i++) {
+              AbstractNode child = children.remove(RandomProvider.getRandom().nextInt(numberOfNodes--));
+              child.getParents().add(parent);
+              parent.getChildren().add(child);
+              newNodes.add(child);
+            }
+          }
+          // vrati samo one koji imaju roditelja
+          return new ArrayList<>(newNodes);
+      }
     }
+    
+    return nodes;
   }
 
   /**
    * Obraduje slucaj kada je L == P
-   * @param nodes cvorovi koji su na trenutnoj razini, spajaju se s roditeljima koji pokazuju na trenutnu razinu
+   * 
+   * @param nodes cvorovi koji su na trenutnoj razini, spajaju se s roditeljima koji pokazuju na
+   *          trenutnu razinu
    * @param levelEdges veze
-   * @param currentLevel 
+   * @param currentLevel
    * @param numberOfNodes
    */
   private void lEqualP(List<AbstractNode> nodes, List<Map<AbstractNode, Integer>> levelEdges, int currentLevel,
       int numberOfNodes) {
-   
+
     List<AbstractNode> children = new LinkedList<>(nodes);
     Map<AbstractNode, Integer> parents = levelEdges.get(currentLevel);
     for (AbstractNode parent : parents.keySet()) {
       for (int i = 0; i < parents.get(parent); i++) {
-        AbstractNode child = children.get(RandomProvider.getRandom().nextInt(numberOfNodes--));
+        AbstractNode child = children.remove(RandomProvider.getRandom().nextInt(numberOfNodes--));
         child.getParents().add(parent);
         parent.getChildren().add(child);
       }
@@ -160,18 +217,27 @@ public class ModelGenerator {
       // 5
       for (int j = 0; j < edgeCount; j++) {
         final int targetLevel = targetLevelDistribution.sample();
-
-        if (levelEdges.get(targetLevel).containsKey(node)) {
-          levelEdges.get(targetLevel).put(node, levelEdges.get(targetLevel).get(node) + 1);
-        } else {
-          levelEdges.get(targetLevel).put(node, 1);
-        }
+        putEdgeInLevelEdges(levelEdges, targetLevel, node);
       }
     }
     // obavezno stvoriti jednu vezu prema razini trenutnaRazina + 1
     if (levelEdges.get(currentLevel + 1).isEmpty()) {
       AbstractNode node = nodes.get(RandomProvider.getRandom().nextInt(nodes.size()));
       levelEdges.get(currentLevel + 1).put(node, 1);
+    }
+  }
+
+  /**
+   * Stvara novu vezu.
+   * @param levelEdges lista koja cuva sve trenutne veze
+   * @param targetLevel razina na koju pokazuje veza
+   * @param node roditelj veze
+   */
+  private void putEdgeInLevelEdges(List<Map<AbstractNode, Integer>> levelEdges, int targetLevel, AbstractNode node) {
+    if (levelEdges.get(targetLevel).containsKey(node)) {
+      levelEdges.get(targetLevel).put(node, levelEdges.get(targetLevel).get(node) + 1);
+    } else {
+      levelEdges.get(targetLevel).put(node, 1);
     }
   }
 
