57b8b06002f4feb2f46d0ae6d3ecbe2dc91247d1
Antun Razum <antun.razum@fer.hr>
2015-02-16 15:27:55 +0100

    DotUtilities updated.

--- src/main/java/hr/fer/zemris/composite/generator/dot/DotUtilities.java
+++ src/main/java/hr/fer/zemris/composite/generator/dot/DotUtilities.java
@@ -1,34 +1,70 @@
 package hr.fer.zemris.composite.generator.dot;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import hr.fer.zemris.composite.generator.model.AbstractNode;
+import hr.fer.zemris.composite.generator.model.DirectionType;
 import hr.fer.zemris.composite.generator.model.Model;
+import hr.fer.zemris.composite.generator.model.nodes.OutputNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class DotUtilities {
 
-	public static String toDot(Model model, String graphName) {
-		StringBuilder sb = new StringBuilder();
-		Set<Long> visited = new HashSet<>();
+  public static String toDot(final Model model, final String graphName) {
+    final OutputNode outputNode = model.getOutput();
+    outputNode.calculateReliability(DirectionType.PARENT);
 
-		sb.append("digraph ").append(graphName).append(" {\n");
-		
-		buildString(sb, model.getOutput(), visited);
-		sb.append("}");
+    final Set<AbstractNode> visited = new HashSet<>();
+    getNodes(outputNode, visited);
 
-		return sb.toString();
-	}
+    final StringBuilder builder = new StringBuilder();
 
-	private static void buildString(StringBuilder sb, AbstractNode node, Set<Long> visited) {
-		if (visited.contains(node.getId())) {
-			return;
-		}
+    builder.append("digraph ").append(graphName).append(" {\n");
 
-		for (AbstractNode parent : node.getParents()) {
-			buildString(sb, parent, visited);
-			visited.add(parent.getId());
-			sb.append(parent.getId()).append(" -> ").append(node.getId()).append(";\n");
-		}
-	}
+    final List<List<AbstractNode>> levels = new ArrayList<>();
+
+    for (int i = 0; i <= outputNode.getLevel(); i++) {
+      levels.add(new ArrayList<AbstractNode>());
+    }
+
+    for (final AbstractNode node : visited) {
+      levels.get(node.getLevel()).add(node);
+
+      for (final AbstractNode parent : node.getParents()) {
+        builder.append(node.getId()).append(" -> ").append(parent.getId()).append(";\n");
+      }
+    }
+
+    builder.append("\n");
+
+    for (int i = 0; i < levels.size(); i++) {
+      builder.append("subgraph level_" + i + " {\nrank=same;\n");
+
+      for (final AbstractNode node : levels.get(i)) {
+        builder.append(node.getId() + " [label=\"" + node.getType().toString().toLowerCase() + "\\n"
+            + String.format("%.3f", node.getReliability()) + "\"]" + "\n");
+      }
+
+      builder.append(";\n}\n");
+    }
+
+    builder.append("}");
+
+    return builder.toString();
+  }
+
+  private static void getNodes(final AbstractNode node, final Set<AbstractNode> visited) {
+    if (visited.contains(node)) {
+      return;
+    }
+
+    visited.add(node);
+
+    for (final AbstractNode parent : node.getParents()) {
+      getNodes(parent, visited);
+    }
+  }
+
 }
