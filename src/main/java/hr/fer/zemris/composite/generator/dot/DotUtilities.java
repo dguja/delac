822c2ccc6f40106c67d8c406a40cf902bb323c1b
Daniel Guja <daniel.guja@gmail.com>
2015-02-16 13:22:03 +0100

    Dot example.

--- src/main/java/hr/fer/zemris/composite/generator/dot/DotUtilities.java
+++ src/main/java/hr/fer/zemris/composite/generator/dot/DotUtilities.java
@@ -8,25 +8,26 @@ import hr.fer.zemris.composite.generator.model.Model;
 
 public class DotUtilities {
 
-	public static String toDot(Model model) {
+	public static String toDot(Model model, String graphName) {
 		StringBuilder sb = new StringBuilder();
 		Set<Long> visited = new HashSet<>();
+
+		sb.append("digraph ").append(graphName).append(" {\n");
 		
-		sb.append("digraph test {\n");
 		buildString(sb, model.getOutput(), visited);
 		sb.append("}");
-		
+
 		return sb.toString();
 	}
 
 	private static void buildString(StringBuilder sb, AbstractNode node, Set<Long> visited) {
-		if(visited.contains(node.getId())) {
+		if (visited.contains(node.getId())) {
 			return;
 		}
-		
-		for(AbstractNode parent : node.getParents()) {
-			visited.add(parent.getId());
+
+		for (AbstractNode parent : node.getParents()) {
 			buildString(sb, parent, visited);
+			visited.add(parent.getId());
 			sb.append(parent.getId()).append(" -> ").append(node.getId()).append(";\n");
 		}
 	}
